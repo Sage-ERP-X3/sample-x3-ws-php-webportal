7e78ebf381aa7e4db7543079429e9ec154f45a0d
Olivier Mantel (Sage) <olivier.mantel@sage.com>
2021-07-19 18:11:34 +0200

    Update Config.php

--- config/Config.php
+++ config/Config.php
@@ -4,11 +4,11 @@ class Config {
 	/*
 	 	Config SOAP Web services X3 
 	*/
-	public static $WSDL            	= "http://x3erpv12sqlvm:8124/soap-wsdl/syracuse/collaboration/syracuse/CAdxWebServiceXmlCC?wsdl";
+	public static $WSDL            	= "http:.../soap-wsdl/syracuse/collaboration/syracuse/CAdxWebServiceXmlCC?wsdl";
 	public static $CODE_LANG       	= "ENG";
-	public static $CODE_USER       	= "admin";
-	public static $PASSWORD        	= "S@geX3_R0cks";
-	public static $POOL_ALIAS      	= "adc";
+	public static $CODE_USER       	= "...";
+	public static $PASSWORD        	= "...";
+	public static $POOL_ALIAS      	= "...";
 	public static $REQUEST_CONFIG  	= "adxwss.optreturn=XML";
 	public static $WS_ORDER  		= "YOSOH";
 	public static $WS_STOCK  		= "YSTOCK_LOT";
@@ -18,9 +18,9 @@ class Config {
 	 	Config GraphQL X3
 	*/
 
-	public static $GQL_SERVER 	= "http://x3erpv12sqlvm:8124/xtrem/api";
-	public static $GQL_USER   	= "admin";
-	public static $GQL_PASSWORD = "S@geX3_R0cks";
+	public static $GQL_SERVER 	= "http:.../xtrem/api";
+	public static $GQL_USER   	= "...";
+	public static $GQL_PASSWORD = "...";
 
 	/*
 	 	Config PHP Web Portal
