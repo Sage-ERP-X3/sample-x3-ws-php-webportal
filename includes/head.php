ed643bbf3f0156d4ec301524476f1ca41edf33b5
Olivier Mantel <olivier.mantel@sage.com>
2025-08-20 15:54:30 +0200

    Revert "issue #è ok"
    This reverts commit f1c502fb3c65e1166a31dfb12679aecaddd5539a.

--- includes/head.php
+++ includes/head.php
@@ -40,3 +40,9 @@ if (isset ( $_SESSION ["x3login"] )) {
 	href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700'
 	rel='stylesheet' type='text/css'>
 
+<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
